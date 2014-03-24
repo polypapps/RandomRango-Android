51ac8b0b13aaf7d31c87bbf1e923ac21f16da969
Rodrigo Silva dos Santos <rodrigos.santos01@gmail.com>
2014-03-24 17:55:31 -0300

    app style and colors

--- src/com/polypapps/randomrango/MainActivity.java
+++ src/com/polypapps/randomrango/MainActivity.java
@@ -60,12 +60,6 @@ public class MainActivity extends ActionBarActivity
             case 1:
                 mTitle = getString(R.string.title_section1);
                 break;
-            case 2:
-                mTitle = getString(R.string.title_section2);
-                break;
-            case 3:
-                mTitle = getString(R.string.title_section3);
-                break;
         }
     }
 
